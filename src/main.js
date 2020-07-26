a2ce277bf74c4d0bca965bf9e2ecdd8c17b76785
jobbot <joevarela83@gmail.com>
2020-07-26 10:08:52 -0600

    Refactor createCard() and make displayCard()

--- src/main.js
+++ src/main.js
@@ -41,31 +41,45 @@ function displayOff() {
 
 /////////////// iteration 2 ///////////////
 
-function createCard(title, idea) {
+function createCard() {
   var title = inputTitle.value;
   var body = inputBody.value;
   if (!(title === '' || body === '')) {
     var userCard = new Idea(title, body);
     ideas.push(userCard);
-    sectionIdeaCards.insertAdjacentHTML('beforeend',
-      '<article class="card">
-        <section class="card-banner-top">
-          <img class="banner-star" src="assets/star.svg" alt="card-star-icon">
-          <img class="banner-x" src="assets/delete.svg" alt="card-delete-icon">
-        </section>
-        <section class="card-content">
-          <h2 class="card-title">Idea title</h2>
-          <h2 class="card-body">Idea body</h2>
-        </section>
-        <section class="card-footer">
-          <img src="assets/comment.svg" alt="icon-comment">
-          <h3 class="comment-text">Comment</h3>
-        </section>
-      </article>');
+    displayCard(title, body);
   }
 };
 
-//problem: unsure of how to manipulate the DOM using the data model
-//problem: setting the HTML text as an argument for insertAdjacentHTML
-//create empty array --> var.innerHTML += newHTML
-//how do we use the userCard variable while working with innerHTML?
+// ***BUG*** apply margin on the entire on parent container? Margins Don't match when new card is created
+// ***BUG*** Card margin needs to wrap text below and not increase width of card.
+// clearTemplates();
+// potentially research the difference in wrapping the input fields in a form class
+// potentially research the differenct in a submit button vs a regular button
+
+function displayCard(title, body) {
+  clearTemplates();
+  var additionalCard = "";
+  for (var i = 0; i < ideas.length; i++) {
+    additionalCard =
+    `<article class="card">
+      <section class="card-banner-top">
+        <img class="banner-star" src="assets/star.svg" alt="card-star-icon">
+        <img class="banner-x" src="assets/delete.svg" alt="card-delete-icon">
+      </section>
+      <section class="card-content">
+        <h2 class="card-title">${ideas[i].title}</h2>
+        <h2 class="card-body">${ideas[i].body}</h2>
+      </section>
+      <section class="card-footer">
+        <img src="assets/comment.svg" alt="icon-comment">
+        <h3 class="comment-text">Comment</h3>
+      </section>
+    </article>`;
+    sectionIdeaCards.innerHTML += additionalCard;
+  }
+};
+
+function clearTemplates() {
+  sectionIdeaCards.innerHTML = '';
+};
